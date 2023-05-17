910c1fc0acc2dd6ea492e8b955537bd2501e65c8
Walter Schreppers <walter@sitweb.eu>
2023-05-17 14:00:28 +0200

    more mapping done for updating

--- app/services/meta_mapping.py
+++ app/services/meta_mapping.py
@@ -17,11 +17,7 @@ from viaa.configuration import ConfigParser
 from viaa.observability import logging
 
 # TODO: these setters will also need to be refactored
-from app.services.mh_properties import (
-    set_property,
-    set_array_property,
-    set_json_array_property
-)
+from app.services.mh_properties import set_property
 from app.services.xml_sidecar import XMLSidecar
 from app.services.input_escaping import markdown_to_html, cleanup_markdown, escape
 
@@ -73,12 +69,11 @@ def save_json_value(field_type, json_value, value_key):
 
     return result
 
-# TODO: ask if this is ok to return first element of list here.
-# in v1 we had 1 value, here we have an array with 1 value so we just return the first
-# title string to make it compatible
-
 
 def get_title(mam_data, title_name):
+    # TODO: ask if this is ok to return first element of list here.
+    # in v1 we had 1 value, here we have an array with 1 value so we just return the first
+    # title string to make it compatible
     if mam_data.get('Dynamic') is None:
         return ''
 
@@ -238,6 +233,7 @@ class MetaMapping:
                 'dottedKey': None
             }
 
+    # TODO: convert this also to v2 format
     def update_legacy_flag(self, request, mam_data):
         # default waarde voor lom_legacy
         lom_legacy = "true"
@@ -265,8 +261,7 @@ class MetaMapping:
         # update fields we are allowed to alter in v2 structure:
         # mam_data['Dynamic']['PID'] = pid
         mam_data['Dynamic']['dc_title'] = request.form.get('ontsluitingstitel')
-        mam_data['Dynamic']['dcterms_issued'] = request.form.get(
-            'uitzenddatum')
+        mam_data['Dynamic']['dcterms_issued'] = request.form.get('uitzenddatum')
         mam_data['Dynamic']['dcterms_abstract'] = cleanup_markdown(
             request.form.get('avo_beschrijving')
         )
@@ -304,11 +299,6 @@ class MetaMapping:
             'Sleutelwoord', request.form.get('trefwoorden'), 'code'
         )
 
-        # TODO: these still need converting:
-        # print(" >>> mam_data  =", json.dumps(mam_data, indent=2))
-        # print(" >>> FORM DATA =", request.form)
-        # __import__('pdb').set_trace()
-
         mam_data = self.update_legacy_flag(request, mam_data)
 
         dc_creators = []
@@ -329,6 +319,8 @@ class MetaMapping:
                 request.form, 'prd_publisher', f)
             if publisher:
                 dc_publishers.append(publisher)
+
+        __import__('pdb').set_trace()
 
         mam_data = set_property(mam_data, 'dc_creators', dc_creators)
         mam_data = set_property(
