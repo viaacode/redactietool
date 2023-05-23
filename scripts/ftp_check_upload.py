d2019c3929a5823d6938807175f022a3abfeb3a1
Walter Schreppers <walter@sitweb.eu>
2023-05-23 18:51:57 +0200

    ftp is now working again (ftp server issue and passive mode was not needed at all).

--- scripts/ftp_check_upload.py
+++ scripts/ftp_check_upload.py
@@ -5,7 +5,9 @@ print("logging in...")
 ftp = FTP(os.environ.get("FTP_SERVER"), timeout=30)
 ftp.login(os.environ.get('FTP_USER'),  os.environ.get('FTP_PASS'))
 ftp.cwd(os.environ.get('FTP_DIR'))
-ftp.set_pasv(False)
+
+# do not set this anymore
+# ftp.set_pasv(False)
 
 print("uploading...")
 ftp.storbinary('STOR test.srt', fp=open('./test.srt', 'rb'))
