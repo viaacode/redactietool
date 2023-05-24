96ed62e7154e751f24bfedaeb44b3826d0af414b
Walter Schreppers <walter@sitweb.eu>
2023-05-24 15:20:11 +0200

    add some debug prints to fix trefwoorden

--- app/services/meta_mapping.py
+++ app/services/meta_mapping.py
@@ -59,6 +59,9 @@ class MetaMapping:
         if item_type_lom and len(item_type_lom) > 0:
             item_type = item_type_lom[0]['value']
 
+        print("item keywords=",  dynamic_field(mam_data, 'lom_keywords', 'Sleutelwoord'))
+        print("item keywords_cp=", mam_data.get('Descriptive').get('Keywords').get('Keyword'))
+
         return {
             'pid': pid,
             'department': department,
@@ -73,13 +76,11 @@ class MetaMapping:
             'item_onderwijsgraden_legacy': dynamic_field(mam_data, 'lom_typicalagerange', 'multiselect'),
             'item_onderwijsniveaus_legacy': dynamic_field(mam_data, 'lom_context', 'multiselect'),
             'item_keywords': dynamic_field(mam_data, 'lom_keywords', 'Sleutelwoord'),
-            # this is just an educated guess TODO: check if this is now correct
             'item_keywords_cp': mam_data.get('Descriptive').get('Keywords').get('Keyword'),
             'publish_item': 'ajax'  # signal ajax request to frontend
         }
 
     def form_params(self, pid, department, mam_data, errors=[]):
-        # print("frontend data=", json.dumps(mam_data, indent=2))
         keyframe_edit_url = '{}{}'.format(
             os.environ.get('KEYFRAME_EDITING_LINK',
                            'https://set_in_secrets?id='),
